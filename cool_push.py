59956d04fb6db6c0c5c744208386443f449be787
Coupile <1172464137@qq.com>
2021-02-20 13:19:30 +0800

    Update cool_push.py

--- cool_push.py
+++ cool_push.py
@@ -1,8 +1,8 @@
 import requests
 
 def send_msg_to_qq_group(content):
-    group_api = 'https://red-bar-1d38.coupile1.workers.dev'
-    title = "昆山天气"
+    group_api = 'https://**.workers.dev'  #填你自己的workers链接
+    title = "天气推送"
     url= 'https://www.qweather.com/'
     params={
         'title':title,
